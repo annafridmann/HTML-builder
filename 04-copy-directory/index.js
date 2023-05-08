fbe2c55de09bcbe9c342314109509a6745e58b72
github-username <annafridmann@gmail.com>
2023-05-08 12:28:35 +0300

    add: 4th task added

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
+const path = require('path');
+const process = require('process');
 
+const dirFrom = path.join(__dirname, 'files');
+const dirTo = path.join(__dirname, 'files-copy');
+
+fsPromises
+  .mkdir(dirTo, { recursive: true });
+
+fsPromises
+  .readdir(dirFrom, { withFileTypes: true })
+  .then((files) => {
+    files.forEach(filename => {
+      let fromFilePath = path.join(dirFrom, filename.name);
+      let toFilePath = path.join(dirTo, filename.name);
+
+      fs.copyFile(fromFilePath, toFilePath, (err) => {
+        if (err) process.stdout.write(err);
+      });
+    });
+  });
