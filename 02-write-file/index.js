8ac4e2398e4a2115728488c9015183f8861f6482
github-username <annafridmann@gmail.com>
2023-05-09 22:19:45 +0300

    fix: added 2task exit last phrase on ctrlC

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
+
 
 const { stdin, stdout } = process;
 const newFileName = 'destination.txt';
 const newFileWay = path.join(__dirname, newFileName);
 let writableStream = fs.createWriteStream(newFileWay);
+const rl = readline.createInterface(stdin);
+
+const stopWrite = () =>{
+    writableStream.end();
+    rl.close();
+    process.exit();
+}
 
 stdout.write('Напиши что то:\n');
 stdin.on('data', data => {
@@ -13,6 +22,8 @@ stdin.on('data', data => {
     if (data.toString() === 'exit\r\n'){
         process.on('exit', () => 
             console.log('удачи'));
-            process.exit();
+            stopWrite();
     }
 });
+rl.on('close', () =>{console.log('удачи')});
+process.on('SIGINT', stopWrite);
