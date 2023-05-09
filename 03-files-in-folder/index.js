014f6fa3c75f48f2b9e2d19c17ccdfb1d57e62f7
github-username <annafridmann@gmail.com>
2023-05-09 16:04:42 +0300

    fix: 3 task function fixed format output info

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,13 +2,14 @@ const path = require('path');
 const fs = require('fs');
 const folderDir = path.join(__dirname, 'secret-folder');
 
-fs.readdir(folderDir, { withFileTypes: true }, (err, data) => {
-  const fileInfo = data.filter((file) => !file.isDirectory());
-  fileInfo.map((file) => {
+fs.readdir(folderDir, {withFileTypes: true}, (err, files) => {
+  console.log("\nCurrent directory files:");
+  files.forEach(file =>{
     const filePath = path.join(folderDir, file.name);
-    fs.stat(filePath, (err, data) => {
+    fs.stat(filePath, (err, data) =>{
       const [name, ext] = path.basename(filePath).split('.');
       console.log(`${name} - ${ext} - ${data.size / 1000} kb`);
     });
+    // console.log();
   });
 });
